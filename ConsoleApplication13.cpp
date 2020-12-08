04b0726e96fbcbea4d2248df3a1fca440aa207d9
Oleksandr Zabila <71663207+OLZABILA@users.noreply.github.com>
2020-12-08 15:16:59 +0200

    Add files via upload

---
+++ ConsoleApplication13.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	char name[1000];
+	char name1[1000];
+	cin >> name;
+	cin >> name1;
+	for (int i = 99; i >= 0; --i)// щоб перевернути перевернуту i--
+	{
+		cout << i[name];
+	}
+	for (int i = 99; i >= 0; --i)
+	{
+		cout << i[name1];
+	}
+}
